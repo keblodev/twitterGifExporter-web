10df603fd54b0f95b51869c07c5e248dbb96bd23
Ronan Amsterdam <romanfromrome.com@gmail.com>
2020-01-29 13:14:10 +0800

    deps updated

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './app';
 
 import './style/index.less';
 
-ReactDOM.render(<App />, document.body);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // register ServiceWorker via OfflinePlugin, for prod only:
 if (process.env.NODE_ENV==='production') {
@@ -15,5 +15,3 @@ if (process.env.NODE_ENV==='production') {
 if (module.hot) {
     module.hot.accept('./containers/Root-ENV_TARGET', () => requestAnimationFrame(init) );
 }
-
-init();
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,4 @@
 import webpack                      from 'webpack';
-import ExtractTextPlugin            from 'extract-text-webpack-plugin';
 import HtmlWebpackPlugin            from 'html-webpack-plugin';
 import ScriptExtHtmlWebpackPlugin   from 'script-ext-html-webpack-plugin';
 import autoprefixer                 from 'autoprefixer';
